c9a6719933a601c3043bfc881369dbbcf2e5df83
DomingosCesar <domingosarmandotavarescesar99@gmail.com>
2023-10-10 22:23:31 +0100

    changed style file

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -105,6 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+# STATIC_ROOT = BASE_DIR.joinpath('static/')
 STATICFILES_DIRS = [BASE_DIR.joinpath('static/')]
 
 MEDIA_URL = '/media/'
