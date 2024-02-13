27f52644d2fa83bc78c340fc424c13827678294d
DomingosCesar <domingosarmandotavarescesar99@gmail.com>
2024-02-13 18:44:56 +0100

    add data

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -144,7 +144,7 @@ USE_TZ = True
 import os
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR.joinpath('static/')
+# STATIC_ROOT = BASE_DIR.joinpath('static/')
 # STATICFILES_DIRS =  (os.path.join(BASE_DIR, "static"), )
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
